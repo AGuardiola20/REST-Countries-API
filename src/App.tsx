a7b43f24214ea9df3ad4e4e1031a4e37dec03d38
AGuardiola20 <andres.pguardiola@gmail.com>
2024-11-27 22:52:48 -0600

    feat: getCountry

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import Card from "./components/Card";
+import { useCountry } from "./hooks/useCountry";
 
 const Container = styled.div`
   background-color: ${({ theme }) => theme.background};
@@ -7,6 +8,13 @@ const Container = styled.div`
 `;
 
 function App() {
+  const { data, error, loading } = useCountry("/alpha/USA");
+
+  // TODO: Mejorar el estilo de los estados de error y loading
+  if (loading) return <p>Cargando...</p>;
+  if (error) return <p>Error: {error}</p>;
+  if (!data || data.length === 0) return <p>No se encontraron datos.</p>;
+
   return (
     <Container>
       <Card
---
+++ src/hooks/useCountry.ts
@@ -0,0 +1,47 @@
+import { useEffect, useState } from "react";
+import { Country } from "../types/types";
+
+interface UseCountryState {
+  data: Country[] | null;
+  loading: boolean;
+  error: string | null;
+}
+
+export const useCountry = (endpoint: string) => {
+  const [state, setState] = useState<UseCountryState>({
+    data: null,
+    loading: true,
+    error: null,
+  });
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          `https://restcountries.com/v3.1${endpoint}`
+        );
+        if (!response.ok) {
+          throw new Error(`Error: ${response.statusText}`);
+        }
+
+        const result = await response.json();
+        const filteredData: Country[] = result.map((country: Country) => ({
+          name: country.name,
+          population: country.population,
+          region: country.region,
+          capital: country.capital,
+          flags: country.flags,
+        }));
+
+        setState({ data: filteredData, loading: false, error: null });
+      } catch (error: unknown) {
+        const errorMessage =
+          error instanceof Error ? error.message : "Error desconocido";
+        setState({ data: null, loading: false, error: errorMessage });
+      }
+    };
+    fetchData();
+  }, [endpoint]);
+
+  return state;
+};
